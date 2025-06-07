2f0fc4fadc252b82ba64d633450b89081d42afe7
yemishi <daviddperera10@gmail.com>
2025-06-07 23:00:05 +0100

    add file to export ui components

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,5 +1,10 @@
 export { default as Input } from "./input/Input";
 export { default as InputPhone } from "./input/InputPhone";
+export { default as InputBRL } from "./input/InputBRL";
+export { default as InputNumber } from "./input/InputNumber";
+
+export { default as Select } from "./select/Select";
+
 export { default as Image } from "./image/Image";
 export { default as StepDesc } from "./stepDesc/StepDesc";
 export { default as Loading } from "./loading/Loading";
