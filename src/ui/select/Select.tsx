8e0a6f6e52f177584b30985a9847ec152aacc515
yemishi <daviddperera10@gmail.com>
2025-06-07 23:03:10 +0100

    add custom Select element

---
+++ src/ui/select/Select.tsx
@@ -0,0 +1,27 @@
+import { SelectHTMLAttributes, forwardRef } from "react";
+import { clsMerge } from "@/helpers";
+
+type SelectProps = SelectHTMLAttributes<HTMLSelectElement>;
+
+const Select = forwardRef<HTMLSelectElement, SelectProps>(({ className, children, ...props }, ref) => {
+  return (
+    <select
+      ref={ref}
+      {...props}
+      className={clsMerge(
+        `rounded-lg border border-primary/30 cursor-pointer bg-accent text-dark font-medium px-4 py-2
+         text-sm md:text-base shadow-sm
+         focus:ring-2 focus:ring-secondary focus:border-secondary focus:outline-none
+         transition-all duration-150 ease-in-out
+         disabled:opacity-50 disabled:cursor-not-allowed hover:border-primary`,
+        className
+      )}
+    >
+      {children}
+    </select>
+  );
+});
+
+Select.displayName = "Select";
+
+export default Select;
