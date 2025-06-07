ee056989af1961150ca1f290b44b51561dd0e103
yemishi <daviddperera10@gmail.com>
2025-06-07 22:58:25 +0100

    add file to export custom hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,3 @@
 export { default as useForm } from "./useForm/useForm";
+export { default as useCart } from "./useCart/useCart";
+export { default as useScrollQuery } from "./useScrollQuery/useScrollQuery";
