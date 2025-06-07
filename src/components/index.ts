8e3c02dacac9a693ade2deb0ad24eafee7a8fdc3
yemishi <daviddperera10@gmail.com>
2025-06-07 22:56:34 +0100

    add file to export reusable components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,12 @@
 export { default as Modal } from "./modal/Modal";
+export { default as PopConfirm } from "./modal/popConfirm/PopConfirm";
+export { default as ErrorWrapper } from "./errorWrapper/ErrorWrapper";
+
+export { default as ProductGrid } from "./productGrid/ProductGrid";
+export { default as ProductCard } from "./productCard/ProductCard";
+
+export { default as Header } from "./header/Header";
+export { default as Cart } from "./cart/Cart";
+
+export { default as Login } from "./login/Login";
+export { default as Checkout } from "./checkout/Checkout";
