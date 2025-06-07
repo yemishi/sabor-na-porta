4f9eae46d28b13eca7bc6385254464db360cebdd
yemishi <daviddperera10@gmail.com>
2025-06-07 23:08:27 +0100

    add userType file

---
+++ src/types/userType.ts
@@ -0,0 +1,16 @@
+export type User = {
+  id: string;
+  phone: string;
+  isAdmin: boolean;
+  name: string;
+  address?: Address;
+};
+
+export type Address = {
+  street: string;
+  neighborhood: string;
+  houseNumber: string;
+  city: string;
+  cep: string;
+  complement?: string;
+};
