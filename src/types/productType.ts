7b873c62e2c31908853e1d5650f0772fc42e7b3e
yemishi <daviddperera10@gmail.com>
2025-06-07 23:05:03 +0100

    add productType file

---
+++ src/types/productType.ts
@@ -0,0 +1,23 @@
+export type Product = {
+  id: string;
+  name: string;
+  picture: string;
+  category: string;
+  variants: ProductVariant[];
+  highlights?: string[];
+};
+
+export type ProductVariant = {
+  id: string;
+  name: string;
+  stock: number;
+  price: number;
+  desc: string;
+  addons: AddOn[];
+  promotion?: number;
+};
+
+export type AddOn = {
+  name: string;
+  price: number;
+};
