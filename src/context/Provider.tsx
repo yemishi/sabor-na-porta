c8dae064f49fa612e5a6278dd6a46a6aaa2e9f14
yemishi <daviddperera10@gmail.com>
2025-06-07 22:57:41 +0100

    add contexts providers

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,7 +1,35 @@
 "use client";
+import { Cart } from "@/types";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { createContext, ReactNode, useContext, useState } from "react";
+const queryClient = new QueryClient();
+
+const CartContext = createContext<{
+  cart: Cart;
+  setCart: (cart: Cart) => void;
+} | null>(null);
+
+export const useCartContext = () => {
+  const ctx = useContext(CartContext);
+  if (!ctx) throw new Error("useCartContext must be used within a CartProvider");
+  return ctx;
+};
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [cart, setCart] = useState<Cart>([]);
+
+  return <CartContext value={{ cart, setCart }}>{children}</CartContext>;
+};
 
 export default function Provider({ children, session }: { children: React.ReactNode; session: Session | null }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>
+        <SessionProvider session={session}>{children}</SessionProvider>
+      </CartProvider>
+    </QueryClientProvider>
+  );
 }
