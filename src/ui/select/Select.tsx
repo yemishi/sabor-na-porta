26a5bc0126da1feb3e8f37076b9917c64ae99a45
yemishi <daviddperera10@gmail.com>
2025-06-10 00:01:47 +0100

    updated select component

--- src/ui/select/Select.tsx
+++ src/ui/select/Select.tsx
@@ -9,12 +9,12 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(({ className, children
       ref={ref}
       {...props}
       className={clsMerge(
+        className,
         `rounded-lg border border-primary/30 cursor-pointer bg-accent text-dark font-medium px-4 py-2
          text-sm md:text-base shadow-sm
          focus:ring-2 focus:ring-secondary focus:border-secondary focus:outline-none
          transition-all duration-150 ease-in-out
-         disabled:opacity-50 disabled:cursor-not-allowed hover:border-primary`,
-        className
+         disabled:opacity-50 disabled:cursor-not-allowed hover:border-primary`
       )}
     >
       {children}
