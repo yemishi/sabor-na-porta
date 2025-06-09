4011881ab5eb32cf35dfa2b1b40bcc405f9c04fe
yemishi <daviddperera10@gmail.com>
2025-06-10 00:00:56 +0100

    add formatDate helper

---
+++ src/helpers/formatDate.ts
@@ -0,0 +1,11 @@
+export default function formatDate(date: string | Date): string {
+  const d = new Date(date);
+
+  return d.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
