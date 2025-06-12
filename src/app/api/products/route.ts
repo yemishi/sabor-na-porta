6ac0ac3513f0aec133b0e95f3b0c511978895e5f
yemishi <daviddperera10@gmail.com>
2025-06-12 20:43:01 +0100

    formatted code

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -7,39 +7,44 @@ export async function GET(req: NextRequest) {
   const q = req.nextUrl.searchParams.get("q") || "";
   const highlights = req.nextUrl.searchParams.get("highlights");
   const category = req.nextUrl.searchParams.get("category") || "";
-  const getAll = req.nextUrl.searchParams.get("getAll") || false;
+  const promo = req.nextUrl.searchParams.get("promo") === "true";
 
   try {
-    if (getAll) {
-      const products = await db.product.findMany();
-      return NextResponse.json({ products });
-    }
     const skip = page * take;
     const filters = [{ name: { contains: q, mode: "insensitive" } }] as any;
 
     if (category) {
       filters.push({ category: { contains: category, mode: "insensitive" } });
     }
-    if (highlights) {
+
+    if (promo) {
+      filters.push({ variants: { some: { promotion: { gt: 0 } } } }, { highlights: { isEmpty: false } });
+    }
+    if (highlights && !promo) {
       filters.push({
-        AND: [{ highlights: { isEmpty: false } }],
+        highlights: { isEmpty: false },
       });
     }
 
-    const [products, count] = await Promise.all([
+    const [allFilteredProducts, count] = await Promise.all([
       db.product.findMany({
+        where: {
+          AND: filters,
+        },
         take,
         skip,
+      }),
+      db.product.count({
         where: {
           AND: filters,
         },
       }),
-      db.product.count({ where: { AND: filters } }),
     ]);
-    return NextResponse.json({ products, hasMore: count > take * (page + 1) });
+    console.log(count > take * (page + 1));
+    return NextResponse.json({ products: allFilteredProducts, hasMore: count > take * (page + 1) });
   } catch (error) {
     console.error("Failed to fetch product:", error);
-    return NextResponse.json({ message: "An unexpected error occurred. Please try again later" }, { status: 500 });
+    return NextResponse.json({ message: "Houve problema ao tentar buscar produtos" }, { status: 500 });
   }
 }
 
