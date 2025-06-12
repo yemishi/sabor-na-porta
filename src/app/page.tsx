d7950a392268f2271ce61fb0500197b63d0a2ac4
yemishi <daviddperera10@gmail.com>
2025-06-12 22:03:26 +0100

    add a new state for query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@
 import { ErrorWrapper, ProductGrid } from "@/components";
 import { useScrollQuery } from "@/hooks";
 import { Product } from "@/types";
-import { Loading } from "@/ui";
+import { Image, Loading } from "@/ui";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
-import { CategoryPicker } from "./categoryPicker/CategoryPicker";
+import zoomIcon from "@/assets/icons/zoom.svg";
+import { CategoryPicker, Search } from "./components";
 
 export default function Page() {
   const [category, setCategory] = useState("");
+  const [search, setSearch] = useState("");
+
   const { data: categoryData, isLoading: loadingCategories } = useQuery({
     queryKey: ["products-categories"],
     queryFn: async () => {
@@ -27,28 +30,43 @@ export default function Page() {
     error,
     values: products,
   } = useScrollQuery<Product>({
-    url: `/api/products?${category === "promo" ? `promo=${true}` : `category=${category}`}`,
-    queryKey: ["products", category],
+    url: `/api/products?${category === "promo" ? `promo=${true}` : `category=${category}`}&q=${search}`,
+    queryKey: ["products", category, search],
   });
-  if (isLoading) return <Loading isPage />;
   return (
     <div className="flex flex-col md:flex-row md:gap-6 p-4 md:p-6">
+      {loadingCategories ? (
+        <div>
+          <Loading className="mb-5" spinBorderColor="border-blue-500" isPage={false} />
+        </div>
+      ) : (
+        <div className="md:w-[14rem] h-full flex-shrink-0 md:sticky md:mt-30 md:top-40 md:self-start">
+          <CategoryPicker categories={categories as string[]} current={category} onChange={setCategory} />
+        </div>
+      )}
+      <Search value={search} handleValue={(v: string) => setSearch(v)} />
       <ErrorWrapper error={isError} message={error?.message}>
-        {loadingCategories ? (
+        {isLoading ? (
           <div>
-            <Loading isPage={false} />
+            <Loading className="mt-5" isPage={false} />
           </div>
         ) : (
-          <div className="md:w-[14rem] h-full flex-shrink-0 md:sticky md:mt-30 md:top-40 md:self-start">
-            <CategoryPicker categories={categories as string[]} current={category} onChange={setCategory} />
-          </div>
+          <>
+            {products.length > 0 ? (
+              <div className="flex flex-col gap-3 flex-1 p-2 md:p-0">
+                <ProductGrid products={products} />
+              </div>
+            ) : (
+              <div className="flex flex-col items-center justify-center gap-2 text-center text-muted mt-10">
+                <Image src={zoomIcon} className="size-12 opacity-50" />
+                <p className="text-lg font-medium">Nenhum produto encontrado</p>
+                <p className="text-sm text-gray-500">Tente ajustar os filtros ou procurar por outro nome.</p>
+              </div>
+            )}
+            {isFetchingNextPage && <Loading className="mt-5" isPage={false} />}
+            {!isFetchingNextPage && hasNextPage && <div ref={ref} />}
+          </>
         )}
-        <div className="flex flex-col gap-3 flex-1 p-2 md:p-0">
-          <ProductGrid products={products} />
-        </div>
-
-        {isFetchingNextPage && <Loading isPage={false} />}
-        {!isFetchingNextPage && hasNextPage && <div ref={ref} />}
       </ErrorWrapper>
     </div>
   );
